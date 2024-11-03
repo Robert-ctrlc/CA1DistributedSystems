8aa667f01c41b38446c9eb9b4ee1aaa32bc54ba5
Robert <60601627+Robert-ctrlc@users.noreply.github.com>
2024-11-03 11:45:18 +0000

    adding classes

---
+++ CA1DistributedSystems/WEB-INF/src/com/example/dit/model/Student.java
@@ -0,0 +1,87 @@
+package com.example.dit.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+public class Student implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String studentNumber;
+    private String phoneNumber;
+    private String address;
+    private String programmeCode;
+
+    @OneToOne(mappedBy = "student", cascade = CascadeType.ALL)
+    private Loan loan;
+
+
+    public Student() {}
+
+    public Student(String name, String studentNumber, String phoneNumber, String address, String programmeCode) {
+        this.name = name;
+        this.studentNumber = studentNumber;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+        this.programmeCode = programmeCode;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStudentNumber() {
+        return studentNumber;
+    }
+
+    public void setStudentNumber(String studentNumber) {
+        this.studentNumber = studentNumber;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getProgrammeCode() {
+        return programmeCode;
+    }
+
+    public void setProgrammeCode(String programmeCode) {
+        this.programmeCode = programmeCode;
+    }
+
+    public Loan getLoan() {
+        return loan;
+    }
+
+    public void setLoan(Loan loan) {
+        this.loan = loan;
+    }
+}
